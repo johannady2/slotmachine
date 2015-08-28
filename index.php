e655faadab47f32d659e922e9ef91ca22c03cf52
Johanna Cristine Dy <johannady2@gmail.com>
2015-08-29 05:54:03 +1100

    subtracts one ticket each time playbutton is clicked
    - discuss with Sir Wayne about plan for video ads app
    - discuss with Sir Wayne plan for lucky8 game
    - make game responsive
    - add input fields
    - computes number of tickets per Receipt Amount
    - removes decimal places in displayed tickets without rounding off
    - disable editing of receipt amount until remaining tickets are used up.
    - subtracts one ticket each time playbutton is clicked

--- index.php
+++ index.php
@@ -63,6 +63,30 @@
 			
 			</div>
 			<div class="machine-bottom">
+				<div class="row">
+					<div class="col-half">
+						<label for="ReceiptNumber" class="inputfieldlabel"> Receipt Number:</label>
+						<input type="text" placeholder="Receipt Number" class="inputfield" id="ReceiptNumber" name="ReceiptNumber">
+					</div>
+					<div class="col-half">
+						<label for="ReceiptAmount" class="inputfieldlabel"> Receipt Amount:</label>
+						<input type="text" placeholder="Receipt Amount" class="inputfield" id="ReceiptAmount" name="ReceiptAmount">
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				<div class="row">
+					<div class="col-half">
+					
+								<label for="Tickets" class="inputfieldlabel2"> Tickets:</label>
+					
+								<input type="text" value="0" class="inputfield2" id="Tickets" name="Tickets" disabled>
+					
+					</div>
+					<div class="col-half">
+						<label for="RemainingTickets" class="inputfieldlabel2"> Remaining Tickets:</label>
+						<input type="text" value="0" class="inputfield2" id="RemainingTickets" name="Remaining Tickets" disabled>
+					</div>
+				</div>
 			</div>
 		</div>
 		
@@ -82,26 +106,73 @@
   
 
 <script>
+
+
 var spinnerSlotOneResult = 0;
 var spinnerSlotTwoResult = 0;
+var leverclickcounter = 0;
+
 
 
 
 $('.lever-btn').on('click', function()
 {
+	leverclickcounter ++;
+	
 	spinnerSlotOneResult = 0;//reset
 	spinnerSlotTwoResult = 0;//reset
 	spinnerSlotThreeResult = 0;//reset
 
-	
-	resetSlots();
-	//GrandWin();
-	//lose();
-	major();
 
-	 $(this).animate({top: '+=450px'}, 500);
-	$(this).animate({top: '-=450px'}, 1000);
+		if(leverclickcounter == 1)
+		{	
+			$('#ReceiptAmount').attr('disabled','disabled');//.removeAttr('disabled');
+		}
+		
+		var remainingtickets = $('#RemainingTickets').val();
+		
+		$('#RemainingTickets').val(remainingtickets -1);
+		
+		
+		if($('#RemainingTickets').val() == 0)
+		{
+			$('#ReceiptAmount').removeAttr('disabled');
+		}
+		resetSlots();
+		//GrandWin();
+		//lose();
+		//majorWin();
+		minorWin();
+		
+		var windowwidth = $(window).width();
+		//Please keep these the same as mediaqueries in style.css
+		
+
+		if(windowwidth > 632)
+		{
+			leverDownAnimation(420,420);
+		}
+		else if(windowwidth <= 632 && windowwidth > 567)
+		{
+			leverDownAnimation(400,400);
+		}
+		else if(windowwidth <= 567 && windowwidth > 497)
+		{
+			leverDownAnimation(380,380);
+		}
+		else if((windowwidth <= 497 && windowwidth > 425)
+					|| (windowwidth <= 425 && windowwidth > 373))
+		{
+			leverDownAnimation(340,340);
+		}
+
 });
+
+function leverDownAnimation(down,up)
+{
+		$('.lever-btn').animate({top: '+='+down+'px'}, 500);
+		$('.lever-btn').animate({top: '-='+up+'px'}, 1000);
+}
 
 function spinner(whichSlot,durationPerResult,maxSpinTimes,randomResult)
 {
@@ -176,14 +247,16 @@ spinner("ul.slot-one",0,3,arr[0]);
 
 }
 
-function major()
+function majorWin()
 {
 
 var arr = []
-while(arr.length < 3){
+while(arr.length < 1)
+{
   var randomnumber=Math.ceil(Math.random()*7)
   var found=false;
-  for(var i=0;i<arr.length;i++){
+  for(var i=0;i<arr.length;i++)
+  {
 	if(arr[i]==randomnumber){found=true;break}
   }
   if(!found)arr[arr.length]=randomnumber;
@@ -223,8 +296,80 @@ function GrandWin()
 	}, 1200);
 	
 
+}
+
+function minorWin()
+{
+	var winType = Math.floor(Math.random() * 3) + 1;
+	var arr = []
+	while(arr.length < 2)
+	{
+	  var randomnumber=Math.ceil(Math.random()*8)
+	  var found=false;
+	  for(var i=0;i<arr.length;i++)
+	  {
+		if(arr[i]==randomnumber){found=true;break}
+	  }
+	  if(!found)arr[arr.length]=randomnumber;
+	}
 
 	
+	if(winType == 1)
+	{
+		spinner("ul.slot-one",0,3,arr[0]);
+	
+	
+		setTimeout(function()
+		{
+			//GrandWinSpinner("ul.slot-two",100,4);
+			spinner("ul.slot-two",100,4,arr[0]);
+		}, 800);
+		
+		setTimeout(function()
+		{
+
+			//GrandWinSpinner("ul.slot-three",120,7);
+			spinner("ul.slot-three",100,4,arr[1]);
+		}, 1200);
+	}
+	else if(winType == 2)
+	{
+		spinner("ul.slot-one",0,3,arr[0]);
+	
+	
+		setTimeout(function()
+		{
+			//GrandWinSpinner("ul.slot-two",100,4);
+			spinner("ul.slot-two",100,4,arr[1]);
+		}, 800);
+		
+		setTimeout(function()
+		{
+
+			//GrandWinSpinner("ul.slot-three",120,7);
+			spinner("ul.slot-three",100,4,arr[0]);
+		}, 1200);
+	}
+	else if(winType == 3)
+	{
+		spinner("ul.slot-one",0,3,arr[1]);
+	
+	
+		setTimeout(function()
+		{
+			//GrandWinSpinner("ul.slot-two",100,4);
+			spinner("ul.slot-two",100,4,arr[0]);
+		}, 800);
+		
+		setTimeout(function()
+		{
+
+			//GrandWinSpinner("ul.slot-three",120,7);
+			spinner("ul.slot-three",100,4,arr[0]);
+		}, 1200);
+	}
+	
+
 }
 
 function resetSlots()
@@ -236,7 +381,66 @@ function resetSlots()
 
 
 	
+$('#ReceiptAmount').on('input',function()
+{
 
+	if($(this).val().length >= 1)
+	{
+		var receiptAmount = $(this).val();
+		var tickets = receiptAmount /1000;
+		
+		var ticketsToString = tickets.toString();
+		if(ticketsToString.indexOf('.') === -1)
+		{
+			$('#Tickets').val(parseInt(ticketsToString));
+			$('#RemainingTickets').val(parseInt(ticketsToString));
+		}
+		else
+		{
+			ticketsToString = ticketsToString.substring(0, ticketsToString.indexOf('.'));
+			$('#Tickets').val(parseInt(ticketsToString));
+			$('#RemainingTickets').val(parseInt(ticketsToString));
+		}
+
+		
+	}
+	else
+	{
+		$('#Tickets').val(0);
+	}
+});
+
+
+/*this will possibly not work in cordova*/
+$('#ReceiptAmount').keydown(function(event)
+{
+
+				if( event.keyCode >= 1 && event.keyCode < 8)
+				{
+					event.preventDefault();
+				}
+				else if( event.keyCode >8 && event.keyCode < 37)
+				{
+					event.preventDefault();
+				}
+				else if(event.keyCode > 37 && event.keyCode < 39)
+				{
+					event.preventDefault();
+				}
+				else if( event.keyCode >39 && event.keyCode < 48)
+				{
+					event.preventDefault();
+				}
+				else if( event.keyCode >57 && event.keyCode < 96)
+				{
+					event.preventDefault();
+				}
+				else if(event.keyCode > 105)
+				{
+					event.preventDefault();
+				}
+				
+});
 
 </script>
   
